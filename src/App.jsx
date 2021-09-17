850773f56b98f86fd837ae137b5bc5a7ffe716b7
Codefreak69 <iamvikashkumar30@gmail.com>
2021-09-17 13:35:39 +0530

    Added all stuffs

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import colorList from "./colorList";
+import { useState } from "react";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const SearchStyle = {
+    width: "30rem",
+    background: " #F2F1F9",
+    padding: "1.3rem",
+    border: "none",
+    borderRadius: "12px",
+    fontSize: "1.5rem",
+  };
+  const searchRes = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Search Color"
+          style={SearchStyle}
+          value={search}
+          onChange={searchRes}
+        />
+      </div>
+      {colorList.filter((val)=>{
+        if(search == ""){
+          return val;
+        }else if(val.name.toLowerCase().includes(search.toLowerCase())){
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="colorContainer" key={key}>
+            <div
+              style={{
+                backgroundColor: val.color,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100px",
+              }}
+            >
+              <p>{val.name}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
---
+++ src/colorList.js
@@ -0,0 +1,47 @@
+const  colorList = [
+    {
+        name : "RED",
+        color : "red"
+    },
+    {
+        name : "Orange",
+        color : "orange"
+    },
+    {
+        name : "Yellow",
+        color : "yellow"
+    },
+    {
+        name : "Brown",
+        color : "brown"
+    },
+    {
+        name : "Green",
+        color : "green"
+    },
+    {
+        name : "Cyan",
+        color : "cyan"
+    },
+    {
+        name : "Blue",
+        color : "blue"
+    },
+    {
+        name : "Violet",
+        color : "violet"
+    },
+    {
+        name : "Purple",
+        color : "purple"
+    },
+    {
+        name : "Indigo",
+        color : "indigo"
+    },
+    {
+        name : "Black",
+        color : "black"
+    },
+]
+export default colorList;
